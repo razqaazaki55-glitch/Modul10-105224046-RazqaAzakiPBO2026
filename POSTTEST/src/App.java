b2bb9ede26488a2601f4f740202a0a85c9e03e6d
Razqa <razqaazaki55@gmail.com>
2026-05-27 04:28:36 +0700

    modul10

--- POSTTEST/src/App.java
+++ POSTTEST/src/App.java
@@ -3,29 +3,28 @@ import java.util.*;
 public class App {
     public static void main(String[] args) {
 
-        // 1. MAP: Menyimpan daftar film
+        //Menyimpan daftar film
         Map<String, Film> daftarFilm = new HashMap<String, Film>();
 
         daftarFilm.put("F01", new Film("Naruto Shippuden", 80000));
         daftarFilm.put("F02", new Film("One Piece", 75000));
         daftarFilm.put("F03", new Film("Dragon Ball Z", 90000));
 
-        // Menyimpan nomor kursi yang sudah dipilih
+        //Menyimpan nomor kursi yang sudah dipilih
         Set<String> nomorKursiyangSudahDipilih = new HashSet<String>();
 
-        // Menyimpan daftar pemesanan simulasi
+        //Menyimpan daftar pemesanan
         List<Pemesanan> daftarPemesanan = new ArrayList<Pemesanan>();
 
-        // Menyimpan riwayat transaksi yang berhasil
+        //Menyimpan riwayat transaksi yang berhasil
         List<Pemesanan> riwayatTransaksi = new ArrayList<Pemesanan>();
 
         // Kasir memilih film berdasarkan kode film
         String kodeFilm = "F01";
 
-        // Mengambil objek film dari Map menggunakan kode film
         Film filmDipilih = daftarFilm.get(kodeFilm);
 
-        // Jika kode film valid, masukkan data pemesanan
+        //Jika kode film benar, masukkan data pemesanan
         if (filmDipilih != null) {
             daftarPemesanan.add(new Pemesanan("Rendi", filmDipilih, "A1"));
             daftarPemesanan.add(new Pemesanan("Budi", filmDipilih, "A2"));
@@ -35,7 +34,7 @@ public class App {
             System.out.println("Kode film tidak ditemukan.");
         }
 
-        // Memproses setiap pemesanan
+        //Memproses setiap pemesanan
         for (Pemesanan pemesanan : daftarPemesanan) {
 
             boolean statusKursi = nomorKursiyangSudahDipilih.add(pemesanan.getNomorKursi());
@@ -44,15 +43,11 @@ public class App {
                 riwayatTransaksi.add(pemesanan);
                 System.out.println("Pemesanan " + pemesanan.getNamaPemesan() + " berhasil.");
             } else {
-                System.out.println(
-                        "Pemesanan " + pemesanan.getNamaPemesan()
-                        + " gagal, kursi " + pemesanan.getNomorKursi()
-                        + " sudah dipilih."
+                System.out.println("Pemesanan " + pemesanan.getNamaPemesan() + " gagal, kursi " + pemesanan.getNomorKursi() + " sudah dipilih."
                 );
             }
         }
 
-        // Menampilkan riwayat transaksi berhasil
         System.out.println("\n=== RIWAYAT TRANSAKSI BERHASIL ===");
 
         for (Pemesanan pemesanan : riwayatTransaksi) {
