aa9c43ceb3e303a40a52424490f46a28272bcd65
Razqa <razqaazaki55@gmail.com>
2026-06-01 19:58:06 +0700

    Modul10

--- THT/src/App.java
+++ THT/src/App.java
@@ -1,12 +1,11 @@
 public class App {
     public static void main(String[] args) {
-        
         System.out.println("=== SISTEM MANAJEMEN GUDANG ===");
         SistemGudang gudang = new SistemGudang();
         //Mendaftarkan 3 barang baru 
         gudang.tambahBarangBaru("B01", "Keyboard Mechanical", "Elektronik", 15);
-        gudang.tambahBarangBaru("B02", "Kursi Ergonomis", "Furnitur", 5);
-        gudang.tambahBarangBaru("B03", "Monitor 24 Inch", "Elektronik", 10);
+        gudang.tambahBarangBaru("B02", "Kursi ", "Furnitur", 5);
+        gudang.tambahBarangBaru("B03", "Monitor 30 Inch", "Elektronik", 10);
         //Melakukan satu kali penambahan Stok 
         gudang.tambahStok("B01", 5);
         //Melakukan satu kali pengurangan Stok
